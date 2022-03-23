b6f4d2959a998b1948954b5139ff20ef56f9faa4
Juan Sebastian Rodriguez Parra <jsebrodriguezp@gmail.com>
2022-03-23 17:54:47 -0500

    finish

---
+++ app.py
@@ -0,0 +1,27 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return '<h1>Hello from Docker! I have been seen {} times.</h1>\n'.format(count)
+
+@app.route("/about")
+def about():
+    return "<h1>About</h1>"
